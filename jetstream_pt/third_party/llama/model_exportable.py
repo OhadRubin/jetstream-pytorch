a9616b694dfc7a603fb9f680b4f131efee79bcf2
Your Name <you@example.com>
2024-12-20 12:01:31 +0000

    ..

--- jetstream_pt/third_party/llama/model_exportable.py
+++ jetstream_pt/third_party/llama/model_exportable.py
@@ -152,7 +152,7 @@ class TransformerBlock(ModuleBase):
       ragged_block_index=None,
   ):
     with jax.named_scope("Attention"):
-      print("x", x.shape)
+      print("x", x.shape, x)
       attn = self.attention.forward(
           self.attention_norm(x),
           freqs_cis,
